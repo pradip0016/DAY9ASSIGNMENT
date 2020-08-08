3887e02b9c547a3834487dfb312248c5d4704a02
pradip0016 <patilpradip980@gmail.com>
2020-08-08 13:29:06 +0530

    refractor

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,3 +2,11 @@
 #Employee wage camputation program
 
 echo "Welcome To EmpWageComputation"
+echo " check employee present or not "
+Random=$((RANDOM%2))
+	if [ $Random -eq 1 ]
+	then
+		echo "Employee is present"
+	else
+		echo "Employee is absent"
+	fi

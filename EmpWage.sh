30087806b55ff1d395bf2f8ddb1283f5671e9d17
pradip0016 <patilpradip980@gmail.com>
2020-08-08 19:39:39 +0530

    add

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,35 +2,39 @@
 #Employee wage camputation program
 
 echo "Welcome To EmpWageComputation"
-empWagePerHr=20
-empFullTime=1
-empPartTime=2
-dayzPerMonth=20
-TotalSalary=0
+EMP_WAGE_PER_HR=20 
+EMP_FULL_TIME=1
+EMP_PART_TIME=2 
+DAYZ_PER_MONTH=20
+TOTAL_MAX_HR=100
+totalSalary=0 
+totalHr=0
 
 echo " check employee working period fulltime or partime or absent "
 
-for (( i=1; i<=$dayzPerMonth; i++  ))
-do	
-	random=$((RANDOM%3))	
-	case $random in 
-	$empFullTime )
-		empHr=8 
-;;
+function WorkingHours ()
+{
+	
+	for (( i=1; i<=$DAYZ_PER_MONTH; i++ ))
+	do
+	random=$((RANDOM%3))
+	case $random in
 
-	$empPartTime )
-		empHr=4 
-;;
+     	$EMP_FULL_TIME )
+		EMP_HR=8 ;;
+
+	$EMP_PART_TIME )
+		EMP_HR=4 ;;
 	
 	* )
-		empHr=0 
-;;
+		EMP_HR=0 ;;
 	esac
-
-	salary=$(( $empWagePerHr * $empHr ))
-	echo "daily sal: = $salary "
-	TotalSalary=$(( $salary + $TotalSalary ))
-	done
+		
+	totalHr=$(($totalHr + $EMP_HR))
+done
+	
 }
 
-echo "Total Salary Per Month  is = $TotalSalary"
+WorkingHours 
+
+ echo "Total work hours is " = $totalHr        

5d65b0622f07ff6d04ccb3236f061fe6e36280e0
pradip0016 <patilpradip980@gmail.com>
2020-08-08 13:50:38 +0530

    add

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,11 +2,17 @@
 #Employee wage camputation program
 
 echo "Welcome To EmpWageComputation"
+empWageperHer=20
 echo " check employee present or not "
 Random=$((RANDOM%2))
 	if [ $Random -eq 1 ]
 	then
 		echo "Employee is present"
+		emphr=8	
 	else
 		echo "Employee is absent"
+		emphr=0	
 	fi
+
+salary= $(( $empWageperHer * $ $emphr ))
+echo $salary

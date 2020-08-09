4f89bbd83e64fc2cbd79eee02fd452490f8637e5
pradip0016 <patilpradip980@gmail.com>
2020-08-09 10:55:13 +0530

    add

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 echo "Welcome To EmpWageComputation"
+
 EMP_WAGE_PER_HR=20 
 EMP_FULL_TIME=1
 EMP_PART_TIME=2 
 DAYZ_PER_MONTH=20
 TOTAL_MAX_HR=100
-
 totalSalary=0 
 totalHr=0
 salary=0
@@ -19,7 +19,7 @@ function WorkingHours ()
 	random=$((RANDOM%3))
 	case $random in
 
-	$EMP_FULL_TIME )
+     	$EMP_FULL_TIME )
 		EMP_HR=8 ;;
 
 	$EMP_PART_TIME )
@@ -28,22 +28,20 @@ function WorkingHours ()
 	* )
 		EMP_HR=0 ;;
 	esac
-
 	totalHr=$(( $totalHr + $EMP_HR ))
-	Salary=$(( $EMP_WAGE_PER_HR * $EMP_HR ))
+	salary=$(( $EMP_WAGE_PER_HR * $EMP_HR ))
+
 
 	totalSalary=$(( $EMP_WAGE_PER_HR * $totalHr  ))
+	wages[i]="$salary"
 
-	ArrayForWages=( $Salary $totalSalary )
-        echo "Daily wages along with total wages" ${ArrayForWages[@]} 
+	echo "wages per day =" ${wages[i]}
 
+	wages[totalSalary]="$totalSalary"
 
-
-
-
-
-	done
+	echo "totalSalary =" ${wages[totalSalary]}
+done
 
 }
 
-	WorkingHours 
+WorkingHours
